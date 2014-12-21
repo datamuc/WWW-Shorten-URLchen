b4d3375d683ac6c8536273f18769a2474cba9936
Danijel Tasov <m@rbfh.de>
2014-12-21 09:39:57 +0100

    scheissndreck

--- lib/WWW/Shorten/URLchen.pm
+++ lib/WWW/Shorten/URLchen.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use base qw( WWW::Shorten::generic Exporter );
 our @EXPORT = qw( makeashorterlink makealongerlink );
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 use Carp;
 
